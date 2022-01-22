9a992e2873caf291fa68e3c7269eb670203511d3
figueiredojoaopedro <neewprojecti@gmail.com>
2022-01-22 14:13:02 -0300

    classes added

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react";
+
+
+const SearchBox = () => {
+
+    return (
+        <div className="pa2">
+            <input className="pa3 ba b--green bg-lightest-blue" placeholder="Search Robots!" type='search'></input>
+        </div>
+    );
+}
+
+export default SearchBox;
